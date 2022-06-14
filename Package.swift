667061d88172aad29830e3b90d6ed2d50f12e386
renevdkooi <renevdkooi@gmail.com>
2022-06-14 17:40:18 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -24,7 +24,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "NFCPassportReader",
-            dependencies: ["OpenSSL"]),
+            dependencies: [
+            .product(name:"openssl_grpc", package: "BoringSSL-GRPC"),
+          ],
     ]
 )
 
